c03b43cc76d5b3410c56967ba8601be869220dfa
nguyenkhang103 <51941040+nguyenkhang103@users.noreply.github.com>
2019-08-05 22:07:26 +0700

    second

--- app/src/main/java/com/example/ungdungdautien/MainActivity.java
+++ app/src/main/java/com/example/ungdungdautien/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.ungdungdautien;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // toan cuc
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // anh xa
+//        tv1= findViewById(R.id.tvHello);
+
+        tv1.setText("Main set text");
     }
 }
